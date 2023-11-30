ee8e0a8c75981c6d75e2e4dff8e1caf2df55f5e1
HO4678 <123117953+HO4678@users.noreply.github.com>
2023-11-29 21:15:36 -0600

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -179,3 +179,28 @@ function displayUpdate(text, color='green') {
     $('#updates').prepend($(`<li style="background-color:${color}">${text}</li>`));
 }
 
+// Get the body element
+const body = document.body;
+
+// Function to toggle themes
+function toggleTheme(theme) {
+    // Remove existing theme classes
+    body.classList.remove('theme-default', 'theme-dark', 'theme-light');
+
+    // Add the selected theme class
+    body.classList.add(theme);
+}
+
+// Toggle theme on button click
+const themeButton = document.getElementById('theme-toggle-button');
+themeButton.addEventListener('click', function () {
+    // Toggle between themes
+    if (body.classList.contains('theme-default')) {
+        toggleTheme('theme-dark');
+    } else if (body.classList.contains('theme-dark')) {
+        toggleTheme('theme-light');
+    } else {
+        toggleTheme('theme-default');
+    }
+});
+
