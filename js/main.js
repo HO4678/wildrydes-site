c034203bc91906fc7cd7adaf2ec0bf0f3c14ded6
HO4678 <123117953+HO4678@users.noreply.github.com>
2023-11-29 22:02:25 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 "use strict";!function(){var n=$("html"),t=function(){$(".btn-menu").on("click",function(t){t.preventDefault(),n.toggleClass("menu-opened")})},e=function(){t()};e()}();
 
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const themeButton = document.getElementById('theme-toggle-button');
+
+    themeButton.addEventListener('click', function () {
+        body.classList.toggle('theme-dark');
+        body.classList.toggle('theme-light');
+    });
+});
