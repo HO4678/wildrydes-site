2a640ef87d335885813bae287116228e75294baa
HO4678 <123117953+HO4678@users.noreply.github.com>
2023-11-29 23:32:51 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";!function(){var n=$("html"),t=function(){$(".btn-menu").on("click",function(t){t.preventDefault(),n.toggleClass("menu-opened")})},e=function(){t()};e()}();
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Theme Toggle
     const body = document.body;
     const themeButton = document.getElementById('theme-toggle-button');
 
@@ -8,4 +9,38 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('theme-dark');
         body.classList.toggle('theme-light');
     });
+
+    // Weather Information
+    const apiKey = 'ab61fa8b06e0e9a5f3d73518cb10a19c';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=yourCity&appid=' + apiKey;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherCondition = data.weather[0].description;
+            const temperature = (data.main.temp - 273.15).toFixed(2);
+
+            updateWeatherDisplay(weatherCondition, temperature);
+            announceWeather(weatherCondition);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            // Handle error display or logging here
+        });
 });
+
+function updateWeatherDisplay(weatherCondition, temperature) {
+    const weatherConditionElement = document.getElementById('weather-condition');
+    const temperatureElement = document.getElementById('temperature');
+
+    weatherConditionElement.textContent = weatherCondition;
+    temperatureElement.textContent = temperature;
+}
+
+function announceWeather(weatherCondition) {
+    // Integrate with AWS Polly to announce weather conditions
+    // You need to implement this function with AWS Polly integration
+    // Similar to the one mentioned in the previous example
+}
+
+
