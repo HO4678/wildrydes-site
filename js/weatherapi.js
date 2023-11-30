daff5a1480c58ecf150cb1440dcf19626604eb88
HO4678 <123117953+HO4678@users.noreply.github.com>
2023-11-29 23:13:05 -0600

    Create weatherapi.js

---
+++ js/weatherapi.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const apiKey = 'YOUR_API_KEY';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=yourCity&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherDescription = data.weather[0].description;
+            const temperature = data.main.temp;
+
+            // Display weather on the page
+            const weatherElement = document.getElementById('weather-info');
+            weatherElement.textContent = `Current Weather: ${weatherDescription}, Temperature: ${temperature}°C`;
+
+            // Use AWS Polly to announce weather conditions
+            announceWeather(weatherDescription);
+        })
+        .catch(error => console.error('Error fetching weather data:', error));
+});
+
+function announceWeather(weatherDescription) {
+    // Use AWS Polly to announce weather conditions
+    // You need to integrate AWS Polly, provide access credentials, and call the service to generate a voice.
+}
